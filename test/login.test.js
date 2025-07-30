f12d5f2cb0fe37348e14cbf5dc2b3c1fa8dcc46b
emanuelefraga <contatomanufraga@gmail.com>
2025-07-29 21:39:10 -0300

    Adicionando testes de transferência

--- test/login.test.js
+++ test/login.test.js
@@ -5,12 +5,12 @@ describe("Login", () => {
     describe("POST /login", () => {
         it('Deve retornar 200 com token em string quando usar credenciais válidas', async () => {
             const resposta = await request("http://localhost:3000")
-            .post("/login")
-            .set("Content-Type", "application/json")
-            .send({
-                'username': 'julio.lima',
-                'senha': '123456',
-            })
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456',
+                })
             
             expect(resposta.status).to.equal(200)
             expect(resposta.body.token).to.be.a('string')
